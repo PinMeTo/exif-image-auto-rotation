8cfbf4dfd27ee512e3a4e8fbcebdd7d4aa39e00d
Morgan Persson <morgan@pinmeto.com>
2019-01-17 10:51:33 +0100

    Fix exception on close, callback is required

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 var fs = require("fs"),
 	exifParser = require('exif-parser'),
-    gm = require('gm'),
-    async = require('async');
+	gm = require('gm'),
+	async = require('async');
 
-function requiresRotating( original, callback ){
+function requiresRotating(original, callback) {
 	//get the exif data
-	fs.open(original, 'r', function (err, fd) {
-		if (err){
+	fs.open(original, 'r', function(err, fd) {
+		if (err) {
 			log(err);
 		}
 
 		// Read first 64K of image file (EXIF data located here)
 		var headSize = 65536;
 		var buffer = new Buffer(headSize);
-		fs.read(fd, buffer, 0, buffer.length, null, function (err, bytesRead, buffer) {
-			fs.close(fd);
-			if (err){
+		fs.read(fd, buffer, 0, buffer.length, null, function(err, bytesRead, buffer) {
+			fs.close(fd, () => { });
+			if (err) {
 				log(err);
 				return callback(false);
 			}
-			try{
+			try {
 				var parser = exifParser.create(buffer);
 				var exifResult = parser.parse();
-				if( exifResult.tags ){
-					if( exifResult.tags.Orientation ){
-						switch( exifResult.tags.Orientation	) {
+				if (exifResult.tags) {
+					if (exifResult.tags.Orientation) {
+						switch (exifResult.tags.Orientation) {
 							case 1: return callback(false); // top-left  - no transform
 							case 2: return callback(true); // top-right - flip horizontal
 							case 3: return callback(true); // bottom-right - rotate 180
@@ -40,45 +40,45 @@ function requiresRotating( original, callback ){
 
 				//default to false
 				return callback(false);
-			}catch(err){
-				return callback( false );
+			} catch (err) {
+				return callback(false);
 			}
 		});
 	});
 }
-function doesFileExist( path, callback ){
-	fs.stat( path, function(err) {
-		if(err == null) {
-			return callback( true );
+function doesFileExist(path, callback) {
+	fs.stat(path, function(err) {
+		if (err == null) {
+			return callback(true);
 		} else {
-			return callback( false );
+			return callback(false);
 		}
 	});
 }
-function log( a ){
-	console.log( a );
+function log(a) {
+	console.log(a);
 }
-module.exports = function( images, callback ){
+module.exports = function(images, callback) {
 	images = images || []; callback = callback || false;
 
-	if( !Array.isArray(images) ){
+	if (!Array.isArray(images)) {
 		images = [images];
 	}
 	//save the image in the correct orientation if required.. this typically happens with apple devices when taking portrait images.
 	//running through each image one at a time to lower cpu overhead.
-	async.forEach(images, function( image, forEachCallback ){
-		doesFileExist(image, function( pass ){
-			if( pass === false ){
+	async.forEach(images, function(image, forEachCallback) {
+		doesFileExist(image, function(pass) {
+			if (pass === false) {
 				return forEachCallback();
 			}
-			requiresRotating( image, function( needsRotating ){
-				if(needsRotating){
-					gm( image )
+			requiresRotating(image, function(needsRotating) {
+				if (needsRotating) {
+					gm(image)
 						.autoOrient()
-						.write( image, function (err) {
+						.write(image, function(err) {
 							if (err) {
-								log( 'Could not write rotated image to disk:' );
-								log( err );
+								log('Could not write rotated image to disk:');
+								log(err);
 							}
 							return forEachCallback();
 						});
@@ -87,13 +87,13 @@ module.exports = function( images, callback ){
 				}
 			});
 		});
-	}, function( err ){
-		if( err ){
-			log( 'Error with the aync foreach:' );
-			log( err );
+	}, function(err) {
+		if (err) {
+			log('Error with the aync foreach:');
+			log(err);
 		}
 		//last but not least run the callback passed to this module, or just return.
-		if( callback ){
+		if (callback) {
 			return callback();
 		} else {
 			return true;
